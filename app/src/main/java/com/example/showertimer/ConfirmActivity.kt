06f4f59c92b639174edeb1e0af500ad804b1b0b2
Eunchae Cho <choeunchae0828@gmail.com>
2024-09-29 04:21:03 +0900

    confirm, start activity 추가

---
+++ app/src/main/java/com/example/showertimer/ConfirmActivity.kt
@@ -0,0 +1,19 @@
+package com.example.showertimer
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm)
+
+        val finishBtn = findViewById<ImageButton>(R.id.ib_button_finish)
+        finishBtn.setOnClickListener {
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/showertimer/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.example.showertimer
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+    }
+}
