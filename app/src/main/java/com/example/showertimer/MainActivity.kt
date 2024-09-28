4f0eb490d6063fc231825d1cbf2f7a9c0015c8fa
Eunchae Cho <choeunchae0828@gmail.com>
2024-09-29 02:40:11 +0900

    splash 작업 중

--- app/src/main/java/com/example/showertimer/MainActivity.kt
+++ app/src/main/java/com/example/showertimer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.showertimer
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,9 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test = 1;
-        val intent = Intent(this, ToothbrushActivity::class.java)
-        startActivity(intent)
-
+        val startBtn = findViewById<ImageButton>(R.id.ib_start)
+        startBtn.setOnClickListener {
+            val intent = Intent(this, ToothbrushActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
