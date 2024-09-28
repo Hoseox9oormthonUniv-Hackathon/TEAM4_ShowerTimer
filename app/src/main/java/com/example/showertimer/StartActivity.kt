628db124a01a764487d8488db1ded9e015bee5cc
Eunchae Cho <choeunchae0828@gmail.com>
2024-09-29 07:52:26 +0900

    앱로고 추가

--- app/src/main/java/com/example/showertimer/StartActivity.kt
+++ app/src/main/java/com/example/showertimer/StartActivity.kt
@@ -8,6 +8,17 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
+
+    private var toothTime = intent.getIntExtra("최종 양치 시간", 3)
+    private var showerTime = intent.getIntExtra("최종 샤워 시간", 15)
+    private var shampooTime = intent.getIntExtra("최종 샴푸 시간", 5)
+    private var shavingTime = intent.getIntExtra("최종 면도 시간", 3)
+    private var cleansingTime = intent.getIntExtra("최종 세안 시간", 2)
+
+//    private var isCleansing = intent.getBooleanExtra("세안 여부", true)
+//    private var isShampoo = intent.getBooleanExtra("샴푸 여부", true)
+//    private var isShaving = intent.getBooleanExtra("면도 여부", true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
@@ -25,4 +36,15 @@ class StartActivity : AppCompatActivity() {
             android.os.Process.killProcess(android.os.Process.myPid()); // 앱 프로세스 종료
         }
     }
+
+//    override fun onPause() {
+//        super.onPause()
+//        toothTime = viewBinding.editTextEt.text.toString()
+//        viewBinding.editTextEt.text.clear()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        val text = memo.toString()
+//    }
 }
