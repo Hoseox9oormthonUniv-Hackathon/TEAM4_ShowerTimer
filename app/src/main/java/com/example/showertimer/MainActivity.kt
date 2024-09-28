a7bf726e454b8ffc421601b0a7f63478c2501546
Eunchae Cho <choeunchae0828@gmail.com>
2024-09-29 02:49:27 +0900

    splash 화면 추가

--- app/src/main/java/com/example/showertimer/MainActivity.kt
+++ app/src/main/java/com/example/showertimer/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.showertimer.databinding.ActivityMainBinding
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val startBtn = findViewById<ImageButton>(R.id.ib_start)
